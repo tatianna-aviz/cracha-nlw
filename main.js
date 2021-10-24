a069b47a1b7a82602b4d4a34c8d4b5c6404e821e
Tatianna de Avíz <90566930+tatianna-aviz@users.noreply.github.com>
2021-10-24 13:20:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+const linksSocialMedia = {
+  github: 'tatianna-aviz',
+  youtube: 'maykbrito',
+  facebook: 'maykbrito',
+  twitter: 'maykbrito',
+  instagram: 'maykbrito'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    //alert(li.childrem[0].href) concerta essa bosta aqui
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userImage.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+getGitHubProfileInfos()
+
+//ARROW FUNCTIONS
+// function nomedafuncao(argumentos){
+
+// }
+// argumentos =>{
+
+// }
